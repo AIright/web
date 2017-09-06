d34c15eb255c5add861da31b51c7524d133b5257
Alright <eml.alexey@gmail.com>
2017-09-06 20:31:49 +0300

    gunicorn first task DONE

---
+++ hello.py
@@ -0,0 +1,17 @@
+from urllib.parse import parse_qs as parser
+bind = "0.0.0.0:8080"
+def app(environ, start_response):
+    """Simplest possible application object"""
+    resp = parser(environ['QUERY_STRING'])
+    data = ''
+    for a in resp:
+        for b in resp.get(a):
+            data = data + '{0}={1}\n'.format(a, b)
+
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data.encode('utf-8')])
--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#!/usr/bin/env bash
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
+sudo /etc/init.d/mysql start
+# gunicorn -c hello.py hello:app
