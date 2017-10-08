4b5d7cfb947475d8604d89071abd6d531b4fecf8
Alright <eml.alexey@gmail.com>
2017-10-08 11:33:08 +0300

    some corrections hello.py

--- hello.py
+++ hello.py
@@ -6,16 +6,14 @@ bind = "0.0.0.0:8080"
 
 def app(environ, start_response):
     """Simplest possible application object"""
-    resp = parser(environ['QUERY_STRING'])
-    data = ''
-    for a in resp:
-        for b in resp.get(a):
-            data = data + '{0}={1}\n'.format(a, b)
-
+#    resp = parser(environ['QUERY_STRING'])
+    data = '\n'.join(environ.get('QUERY_STRING').split('&'))
+    print(str(data))
+    print(str(len(data)))
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
         ('Content-Length', str(len(data)))
     ]
     start_response(status, response_headers)
-    return iter([data.encode('utf-8')])
+    return [data.encode('utf-8')]
