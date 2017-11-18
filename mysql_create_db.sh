0ef18a43c35a04efa88aabff0aeaa421f92655fc
Alright <eml.alexey@gmail.com>
2017-11-18 15:32:43 +0300

    ORM django complited

--- mysql_create_db.sh
+++ mysql_create_db.sh
@@ -2,3 +2,4 @@
 sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS test_db"
 sudo mysql -u root -e "CREATE USER alright IDENTIFIED BY '123'"
 sudo mysql -u root -e "GRANT ALL ON test_db.* TO alright@localhost IDENTIFIED BY '123' with grant option"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost'"
