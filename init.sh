90051a2da055b94e55c920b5f4f5dee1de231888
Aleksei Emelianov <shark@Alekseis-MacBook-Pro.local>
2020-02-09 10:56:26 +0300

    Preparation for deployment

--- init.sh
+++ init.sh
@@ -2,7 +2,5 @@
 sudo ln -s $PWD/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s $PWD/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo ln -s $PWD/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
-# gunicorn -c hello.py wsgi.py
