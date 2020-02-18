883ea8554581f717ea075d7d598ac107dab5a4f6
Aleksei Emelianov <shark@Alekseis-MacBook-Pro.local>
2020-02-18 03:06:50 +0300

    remove useless files

--- mysql_create_db.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS test_db"
-sudo mysql -u root -e "CREATE USER alright IDENTIFIED BY '123'"
-sudo mysql -u root -e "GRANT ALL ON test_db.* TO alright@localhost IDENTIFIED BY '123' with grant option"
-sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost'"
