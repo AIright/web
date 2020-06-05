a87c82183bea36f4d53aac9d4941cd63dc4fe8e0
airigh <eml.alexey@gmail.com>
2020-06-05 18:42:17 +0300

    fix remote deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,10 +10,8 @@ cd $WORKDIR
 rm -r *
 
 #git reset --hard HEAD~
-git clone git@github.com:AIright/web.git -b $BRANCH
-
-docker build -t blog_base:latest -f ./deploy/Dockerfile
-
-docker-compose up -d
+git clone git@github.com:AIright/web.git -b $BRANCH \
+ && docker build -t blog_base:latest ./deploy \
+ && docker-compose up -d
 
 echo http://ec2-"$(curl -s http://checkip.amazonaws.com | sed 's/\./-/g')".eu-central-1.compute.amazonaws.com/
